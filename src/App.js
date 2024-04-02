e895c459183d5af7e5d25dda206661a80d7a7665
Jihee <sjh709@naver.com>
2024-04-02 20:24:45 +0900

    Route 구성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import AppLayout from './layout/AppLayout';
+import Homepage from './pages/Homepage/Homepage';
+import MoviePage from './pages/Movies/MoviePage';
+import MovieDetailPage from './pages/MovieDetail/MovieDetailPage';
+import NotFoundPage from './pages/NotFoundpage/NotFoundPage';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<AppLayout />}>
+          <Route index element={<Homepage />} />
+          <Route path='movies'>
+            <Route index element={<MoviePage />} />
+            <Route path=':id' element={<MovieDetailPage />} />
+          </Route>
+        </Route>
+
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
