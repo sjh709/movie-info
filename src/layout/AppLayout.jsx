f72c354405b6ecedff6c32031003878e8b1fd6ae
Jihee <sjh709@naver.com>
2024-04-02 22:55:15 +0900

    navigation style

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,7 +1,48 @@
 import React from 'react';
+import { Button, Container, Form, Nav, Navbar } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const AppLayout = () => {
-  return <div>navbar</div>;
+  return (
+    <div>
+      <Navbar variant='dark' expand='lg'>
+        <Container fluid>
+          <Navbar.Brand href='#'>
+            <img width={90} src='image/heeflix-logo.png' />
+          </Navbar.Brand>
+          <Navbar.Collapse id='navbarScroll'>
+            <Nav
+              className='me-auto my-2 my-lg-0'
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Link to='/' className='nav-link'>
+                Home
+              </Link>
+              <Link to='/movies' className='nav-link'>
+                Movies
+              </Link>
+            </Nav>
+            <Form className='d-flex'>
+              <Form.Control
+                type='search'
+                placeholder='Search'
+                className='me-2'
+                aria-label='Search'
+              />
+              <Button variant='outline-danger'>
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+              </Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </div>
+  );
 };
 
 export default AppLayout;
