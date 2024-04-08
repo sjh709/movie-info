89f27c78a5bab89e20f3897e0a251e44ab0dba7f
Jihee <sjh709@naver.com>
2024-04-09 04:34:58 +0900

    movie-card-detail 수정

--- src/pages/MovieDetail/components/MovieDetailCredits/MovieDetailCredits.jsx
+++ src/pages/MovieDetail/components/MovieDetailCredits/MovieDetailCredits.jsx
@@ -26,14 +26,18 @@ const MovieDetailCredits = ({ id }) => {
     <Container>
       <Row>
         <Col>
-          <h3 className='mb-3'>Top Billed Cast</h3>
-          <Row className='mb-4 item-align'>
-            {credits?.splice(0, 6).map((item, index) => (
-              <Col key={index} xs='auto' className='mb-3'>
-                <CreditsCard credits={item} />
-              </Col>
-            ))}
-          </Row>
+          {credits?.length > 0 && (
+            <div>
+              <h3 className='mb-3'>Top Billed Cast</h3>
+              <Row className='mb-4 item-align'>
+                {credits?.splice(0, 6).map((item, index) => (
+                  <Col key={index} xs='auto' className='mb-3'>
+                    <CreditsCard credits={item} />
+                  </Col>
+                ))}
+              </Row>
+            </div>
+          )}
         </Col>
       </Row>
     </Container>
